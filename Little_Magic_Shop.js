16b555d74c68fdf367bf172f00005cb4737c91dc
TainakaDrums <riyuazmi@gmail.com>
2019-04-08 14:09:43 +0500

    some changes

--- Little_Magic_Shop.js
+++ Little_Magic_Shop.js
@@ -358,7 +358,7 @@ var Module=typeof Module!=="undefined"?Module:{};var moduleOverrides={};var key;
 		});
 	}
 
-	function loadXHR(resolve, reject, file, tracker) {
+	    function loadXHR(resolve, reject, file, tracker) {  if (file.substr(-5) === '.wasm' || file.substr(-4) === '.pck') { file += '.gz'; var resolve_orig = resolve; resolve = function(xhr) { return resolve_orig(xhr.responseURL.substr(-3) === '.gz' ? { response: pako.inflate(xhr.response),    responseType: xhr.responseType, responseURL: xhr.responseURL, status: xhr.status,   statusText: xhr.statusText } : xhr); }; }
 
 		var xhr = new XMLHttpRequest;
 		xhr.open('GET', file);
@@ -420,5 +420,7 @@ var Module=typeof Module!=="undefined"?Module:{};var moduleOverrides={};var key;
 				reject(new Error("Loading file '" + file + "' was aborted."));
 				break;
 		}
+
+        if (file.substr(-5) === '.wasm' || file.substr(-4) === '.pck') { file += '.gz'; var resolve_orig = resolve; resolve = function(xhr) { return resolve_orig(xhr.responseURL.substr(-3) === '.gz' ? { response: pako.inflate(xhr.response), responseType: xhr.responseType, responseURL: xhr.responseURL, status: xhr.status, statusText: xhr.statusText } : xhr); }; }
 	}
 })();
